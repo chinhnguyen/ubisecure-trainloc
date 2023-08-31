a48fd7c746834dcf7fb0112b598345e4be56ac41
Chinh Nguyen <nguyen.trungchinh@gmail.com>
2023-08-31 10:42:29 +0300

    Add comment for later improvement

--- backend/src/modules/trains/trains.controller.ts
+++ backend/src/modules/trains/trains.controller.ts
@@ -15,6 +15,11 @@ export class TrainsController {
     @Body() trainLocation: TrainLocation
   ): Promise<void> {
     console.log(trainLocation)
+    // TODO: This is for demo purpose only. In reality when number connections are too many,
+    // sending and wait here will timeout the request. We should use a queue system to handle
+    // the sending of messages.
+    // There is also another choice to batch the changes and send them in one message to avoid 
+    // flooding the network.
     await this.wsService.publishTrainLocation(trainNumber, trainLocation)
   }
 }
